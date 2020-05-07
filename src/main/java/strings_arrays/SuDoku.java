1f89581913e8ff1fa6cc69546ae4f1508c88fe04
Nicola Pilcher <nicolapilcher@msn.com>
2020-05-07 14:08:48 +0200

    commiting my Sudoku work from 2017

--- src/main/java/strings_arrays/SuDoku.java
+++ src/main/java/strings_arrays/SuDoku.java
@@ -5,11 +5,11 @@ Su Doku (Japanese meaning number place) is the name given to a popular puzzle co
 but credit must be attributed to Leonhard Euler who invented a similar, and much more difficult, puzzle idea called
 Latin Squares.
 
-The objective of Su Doku puzzles, however, is to replace the blanks (or zeros) in a 9 by 9 grid in
+The objective of SuDoku puzzles, however, is to replace the blanks (or zeros) in a 9 by 9 grid in
 such that each row, column, and 3 by 3 box contains each of the digits 1 to 9. The example shown in sudoku.ong in the
  'resources' folder in the this project shows a typical starting puzzle grid and its solution grid.
 
-A well constructed Su Doku puzzle has a unique solution and can be solved by logic, although it may be necessary to
+A well constructed SuDoku puzzle has a unique solution and can be solved by logic, although it may be necessary to
 employ "guess and test" methods in order to eliminate options (there is much contested opinion over this). The
 complexity of the search determines the difficulty of the puzzle; the example above is considered easy because it can
 be solved by straight forward direct deduction.
@@ -26,6 +26,88 @@ for example, 483 is the 3-digit number found in the top left corner of the solut
 // Extended ASCII characters 179-218
 //enable unicode hex input keyboard hold down option alt key and type unicode
 
-public class SuDoku {
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SuDoku {
+    public static void main(String[] args) {
+        ReadFiles sudokuRead = new ReadFiles();
+        sudokuRead.openSudokuFile();
+        sudokuRead.readSudokuFile();
+        sudokuRead.closeSudokuFile();
+
+    }
 }
+
+
+//    public static void main(String[] args) throws IOException{
+//
+//        try{
+//            Scanner input = new Scanner(new File("~/Documents/codingnomads/advanced_practice/src/main/" +
+//                    "java/resources/sudoku_grid.txt"));
+//            while(input.hasNextLine()){
+//                String message = input.nextLine();
+//                message = message.replace("0", " ");
+//                String store[] = message.split("/");
+//                for(int a = 0; a < store.length; a++){
+//                    if((store[a]).contains("Grid"){
+//                        String t[] = (store[a]).split("-");
+//                        aStore = aStore + "_" + t[1];
+//                    }
+//                    if((store[a]).contains("B-"){
+//                        String t[] = (store[a]).split("-");
+//                        bStore = bStore + "_" + t[1];
+//                    }
+//                }
+//                aFinal = aStore.split("_");
+//                bFinal = bStore.split("-");
+//            }
+//            input.close();
+//        } catch(Exception e){}
+//    }
+//}
+class ReadFiles {
+    String [] grid = new String[10];
+    int i = 0;
+
+    private Scanner readSudoku;
+    public void openSudokuFile() {
+        try {
+            readSudoku = new Scanner(new File("/Users/nicola/Documents/codingnomads/advanced_practice/src" +
+                    "/main/resources/sudoku_grid.txt"));
+        } catch (Exception e) {
+            System.out.println("File not found.");
+        }
+    }
+    public void readSudokuFile() {
+        try {
+            while (readSudoku.hasNext()) {
+                grid[i] = readSudoku.next();
+                System.out.println(grid[i]);
+                i++;
+            }
+        }catch (IndexOutOfBoundsException exc1) {
+            System.out.println("Here is your Sudoku grid. Time to solve it.");
+        }
+    }
+    public void closeSudokuFile() {
+        readSudoku.close();
+    }
+}
+
+
+    //read the sudoku grids from sudoku_grid.txt and pass it to an array
+
+    //print out the array with break lines and ASCII characters to look like the sudoku game board
+
+    //change any 0's to " "
+
+    //set the program to read input from the user to change number in the grid with A2 B4 like commands
+
+    //once there are 9 of each number in the array sout done with that problem and read the next sudoku grid
+
+    // once all of the grids have been completed sum the first 3 numbers of every grid
+
+
---
+++ src/main/java/strings_arrays/SudokuPojo.java
@@ -0,0 +1,908 @@
+package strings_arrays;
+
+/**
+ * Created by nicola on 7/21/17.
+ */
+public class SudokuPojo {
+
+    String grid01 = "";
+    String grid02 = "";
+    String grid03 = "";
+    String grid04 = "";
+    String grid05 = "";
+    String grid06 = "";
+    String grid07 = "";
+    String grid08 = "";
+    String grid09 = "";
+    String grid10 = "";
+    String grid11 = "";
+    String grid12 = "";
+    String grid13 = "";
+    String grid14 = "";
+    String grid15 = "";
+    String grid16 = "";
+    String grid17 = "";
+    String grid18 = "";
+    String grid19 = "";
+    String grid20 = "";
+    String grid21 = "";
+    String grid22 = "";
+    String grid23 = "";
+    String grid24 = "";
+    String grid25 = "";
+    String grid26 = "";
+    String grid27 = "";
+    String grid28 = "";
+    String grid29 = "";
+    String grid30 = "";
+    String grid31 = "";
+    String grid32 = "";
+    String grid33 = "";
+    String grid34 = "";
+    String grid35 = "";
+    String grid36 = "";
+    String grid37 = "";
+    String grid38 = "";
+    String grid39 = "";
+    String grid40 = "";
+    String grid41 = "";
+    String grid42 = "";
+    String grid43 = "";
+    String grid44 = "";
+    String grid45 = "";
+    String grid46 = "";
+    String grid47 = "";
+    String grid48 = "";
+    String grid49 = "";
+    String grid50 = "";
+    String grid01Final[];
+    String grid02Final[];
+    String grid03Final[];
+    String grid04Final[];
+    String grid05Final[];
+    String grid06Final[];
+    String grid07Final[];
+    String grid08Final[];
+    String grid09Final[];
+    String grid10Final[];
+    String grid11Final[];
+    String grid12Final[];
+    String grid13Final[];
+    String grid14Final[];
+    String grid15Final[];
+    String grid16Final[];
+    String grid17Final[];
+    String grid18Final[];
+    String grid19Final[];
+    String grid20Final[];
+    String grid21Final[];
+    String grid22Final[];
+    String grid23Final[];
+    String grid24Final[];
+    String grid25Final[];
+    String grid26Final[];
+    String grid27Final[];
+    String grid28Final[];
+    String grid29Final[];
+    String grid30Final[];
+    String grid31Final[];
+    String grid32Final[];
+    String grid33Final[];
+    String grid34Final[];
+    String grid35Final[];
+    String grid36Final[];
+    String grid37Final[];
+    String grid38Final[];
+    String grid39Final[];
+    String grid40Final[];
+    String grid41Final[];
+    String grid42Final[];
+    String grid43Final[];
+    String grid44Final[];
+    String grid45Final[];
+    String grid46Final[];
+    String grid47Final[];
+    String grid48Final[];
+    String grid49Final[];
+    String grid50Final[];
+
+    public String getGrid01() {
+        return grid01;
+    }
+
+    public void setGrid01(String grid01) {
+        this.grid01 = grid01;
+    }
+
+    public String getGrid02() {
+        return grid02;
+    }
+
+    public void setGrid02(String grid02) {
+        this.grid02 = grid02;
+    }
+
+    public String getGrid03() {
+        return grid03;
+    }
+
+    public void setGrid03(String grid03) {
+        this.grid03 = grid03;
+    }
+
+    public String getGrid04() {
+        return grid04;
+    }
+
+    public void setGrid04(String grid04) {
+        this.grid04 = grid04;
+    }
+
+    public String getGrid05() {
+        return grid05;
+    }
+
+    public void setGrid05(String grid05) {
+        this.grid05 = grid05;
+    }
+
+    public String getGrid06() {
+        return grid06;
+    }
+
+    public void setGrid06(String grid06) {
+        this.grid06 = grid06;
+    }
+
+    public String getGrid07() {
+        return grid07;
+    }
+
+    public void setGrid07(String grid07) {
+        this.grid07 = grid07;
+    }
+
+    public String getGrid08() {
+        return grid08;
+    }
+
+    public void setGrid08(String grid08) {
+        this.grid08 = grid08;
+    }
+
+    public String getGrid09() {
+        return grid09;
+    }
+
+    public void setGrid09(String grid09) {
+        this.grid09 = grid09;
+    }
+
+    public String getGrid10() {
+        return grid10;
+    }
+
+    public void setGrid10(String grid10) {
+        this.grid10 = grid10;
+    }
+
+    public String getGrid11() {
+        return grid11;
+    }
+
+    public void setGrid11(String grid11) {
+        this.grid11 = grid11;
+    }
+
+    public String getGrid12() {
+        return grid12;
+    }
+
+    public void setGrid12(String grid12) {
+        this.grid12 = grid12;
+    }
+
+    public String getGrid13() {
+        return grid13;
+    }
+
+    public void setGrid13(String grid13) {
+        this.grid13 = grid13;
+    }
+
+    public String getGrid14() {
+        return grid14;
+    }
+
+    public void setGrid14(String grid14) {
+        this.grid14 = grid14;
+    }
+
+    public String getGrid15() {
+        return grid15;
+    }
+
+    public void setGrid15(String grid15) {
+        this.grid15 = grid15;
+    }
+
+    public String getGrid16() {
+        return grid16;
+    }
+
+    public void setGrid16(String grid16) {
+        this.grid16 = grid16;
+    }
+
+    public String getGrid17() {
+        return grid17;
+    }
+
+    public void setGrid17(String grid17) {
+        this.grid17 = grid17;
+    }
+
+    public String getGrid18() {
+        return grid18;
+    }
+
+    public void setGrid18(String grid18) {
+        this.grid18 = grid18;
+    }
+
+    public String getGrid19() {
+        return grid19;
+    }
+
+    public void setGrid19(String grid19) {
+        this.grid19 = grid19;
+    }
+
+    public String getGrid20() {
+        return grid20;
+    }
+
+    public void setGrid20(String grid20) {
+        this.grid20 = grid20;
+    }
+
+    public String getGrid21() {
+        return grid21;
+    }
+
+    public void setGrid21(String grid21) {
+        this.grid21 = grid21;
+    }
+
+    public String getGrid22() {
+        return grid22;
+    }
+
+    public void setGrid22(String grid22) {
+        this.grid22 = grid22;
+    }
+
+    public String getGrid23() {
+        return grid23;
+    }
+
+    public void setGrid23(String grid23) {
+        this.grid23 = grid23;
+    }
+
+    public String getGrid24() {
+        return grid24;
+    }
+
+    public void setGrid24(String grid24) {
+        this.grid24 = grid24;
+    }
+
+    public String getGrid25() {
+        return grid25;
+    }
+
+    public void setGrid25(String grid25) {
+        this.grid25 = grid25;
+    }
+
+    public String getGrid26() {
+        return grid26;
+    }
+
+    public void setGrid26(String grid26) {
+        this.grid26 = grid26;
+    }
+
+    public String getGrid27() {
+        return grid27;
+    }
+
+    public void setGrid27(String grid27) {
+        this.grid27 = grid27;
+    }
+
+    public String getGrid28() {
+        return grid28;
+    }
+
+    public void setGrid28(String grid28) {
+        this.grid28 = grid28;
+    }
+
+    public String getGrid29() {
+        return grid29;
+    }
+
+    public void setGrid29(String grid29) {
+        this.grid29 = grid29;
+    }
+
+    public String getGrid30() {
+        return grid30;
+    }
+
+    public void setGrid30(String grid30) {
+        this.grid30 = grid30;
+    }
+
+    public String getGrid31() {
+        return grid31;
+    }
+
+    public void setGrid31(String grid31) {
+        this.grid31 = grid31;
+    }
+
+    public String getGrid32() {
+        return grid32;
+    }
+
+    public void setGrid32(String grid32) {
+        this.grid32 = grid32;
+    }
+
+    public String getGrid33() {
+        return grid33;
+    }
+
+    public void setGrid33(String grid33) {
+        this.grid33 = grid33;
+    }
+
+    public String getGrid34() {
+        return grid34;
+    }
+
+    public void setGrid34(String grid34) {
+        this.grid34 = grid34;
+    }
+
+    public String getGrid35() {
+        return grid35;
+    }
+
+    public void setGrid35(String grid35) {
+        this.grid35 = grid35;
+    }
+
+    public String getGrid36() {
+        return grid36;
+    }
+
+    public void setGrid36(String grid36) {
+        this.grid36 = grid36;
+    }
+
+    public String getGrid37() {
+        return grid37;
+    }
+
+    public void setGrid37(String grid37) {
+        this.grid37 = grid37;
+    }
+
+    public String getGrid38() {
+        return grid38;
+    }
+
+    public void setGrid38(String grid38) {
+        this.grid38 = grid38;
+    }
+
+    public String getGrid39() {
+        return grid39;
+    }
+
+    public void setGrid39(String grid39) {
+        this.grid39 = grid39;
+    }
+
+    public String getGrid40() {
+        return grid40;
+    }
+
+    public void setGrid40(String grid40) {
+        this.grid40 = grid40;
+    }
+
+    public String getGrid41() {
+        return grid41;
+    }
+
+    public void setGrid41(String grid41) {
+        this.grid41 = grid41;
+    }
+
+    public String getGrid42() {
+        return grid42;
+    }
+
+    public void setGrid42(String grid42) {
+        this.grid42 = grid42;
+    }
+
+    public String getGrid43() {
+        return grid43;
+    }
+
+    public void setGrid43(String grid43) {
+        this.grid43 = grid43;
+    }
+
+    public String getGrid44() {
+        return grid44;
+    }
+
+    public void setGrid44(String grid44) {
+        this.grid44 = grid44;
+    }
+
+    public String getGrid45() {
+        return grid45;
+    }
+
+    public void setGrid45(String grid45) {
+        this.grid45 = grid45;
+    }
+
+    public String getGrid46() {
+        return grid46;
+    }
+
+    public void setGrid46(String grid46) {
+        this.grid46 = grid46;
+    }
+
+    public String getGrid47() {
+        return grid47;
+    }
+
+    public void setGrid47(String grid47) {
+        this.grid47 = grid47;
+    }
+
+    public String getGrid48() {
+        return grid48;
+    }
+
+    public void setGrid48(String grid48) {
+        this.grid48 = grid48;
+    }
+
+    public String getGrid49() {
+        return grid49;
+    }
+
+    public void setGrid49(String grid49) {
+        this.grid49 = grid49;
+    }
+
+    public String getGrid50() {
+        return grid50;
+    }
+
+    public void setGrid50(String grid50) {
+        this.grid50 = grid50;
+    }
+
+    public String[] getGrid01Final() {
+        return grid01Final;
+    }
+
+    public void setGrid01Final(String[] grid01Final) {
+        this.grid01Final = grid01Final;
+    }
+
+    public String[] getGrid02Final() {
+        return grid02Final;
+    }
+
+    public void setGrid02Final(String[] grid02Final) {
+        this.grid02Final = grid02Final;
+    }
+
+    public String[] getGrid03Final() {
+        return grid03Final;
+    }
+
+    public void setGrid03Final(String[] grid03Final) {
+        this.grid03Final = grid03Final;
+    }
+
+    public String[] getGrid04Final() {
+        return grid04Final;
+    }
+
+    public void setGrid04Final(String[] grid04Final) {
+        this.grid04Final = grid04Final;
+    }
+
+    public String[] getGrid05Final() {
+        return grid05Final;
+    }
+
+    public void setGrid05Final(String[] grid05Final) {
+        this.grid05Final = grid05Final;
+    }
+
+    public String[] getGrid06Final() {
+        return grid06Final;
+    }
+
+    public void setGrid06Final(String[] grid06Final) {
+        this.grid06Final = grid06Final;
+    }
+
+    public String[] getGrid07Final() {
+        return grid07Final;
+    }
+
+    public void setGrid07Final(String[] grid07Final) {
+        this.grid07Final = grid07Final;
+    }
+
+    public String[] getGrid08Final() {
+        return grid08Final;
+    }
+
+    public void setGrid08Final(String[] grid08Final) {
+        this.grid08Final = grid08Final;
+    }
+
+    public String[] getGrid09Final() {
+        return grid09Final;
+    }
+
+    public void setGrid09Final(String[] grid09Final) {
+        this.grid09Final = grid09Final;
+    }
+
+    public String[] getGrid10Final() {
+        return grid10Final;
+    }
+
+    public void setGrid10Final(String[] grid10Final) {
+        this.grid10Final = grid10Final;
+    }
+
+    public String[] getGrid11Final() {
+        return grid11Final;
+    }
+
+    public void setGrid11Final(String[] grid11Final) {
+        this.grid11Final = grid11Final;
+    }
+
+    public String[] getGrid12Final() {
+        return grid12Final;
+    }
+
+    public void setGrid12Final(String[] grid12Final) {
+        this.grid12Final = grid12Final;
+    }
+
+    public String[] getGrid13Final() {
+        return grid13Final;
+    }
+
+    public void setGrid13Final(String[] grid13Final) {
+        this.grid13Final = grid13Final;
+    }
+
+    public String[] getGrid14Final() {
+        return grid14Final;
+    }
+
+    public void setGrid14Final(String[] grid14Final) {
+        this.grid14Final = grid14Final;
+    }
+
+    public String[] getGrid15Final() {
+        return grid15Final;
+    }
+
+    public void setGrid15Final(String[] grid15Final) {
+        this.grid15Final = grid15Final;
+    }
+
+    public String[] getGrid16Final() {
+        return grid16Final;
+    }
+
+    public void setGrid16Final(String[] grid16Final) {
+        this.grid16Final = grid16Final;
+    }
+
+    public String[] getGrid17Final() {
+        return grid17Final;
+    }
+
+    public void setGrid17Final(String[] grid17Final) {
+        this.grid17Final = grid17Final;
+    }
+
+    public String[] getGrid18Final() {
+        return grid18Final;
+    }
+
+    public void setGrid18Final(String[] grid18Final) {
+        this.grid18Final = grid18Final;
+    }
+
+    public String[] getGrid19Final() {
+        return grid19Final;
+    }
+
+    public void setGrid19Final(String[] grid19Final) {
+        this.grid19Final = grid19Final;
+    }
+
+    public String[] getGrid20Final() {
+        return grid20Final;
+    }
+
+    public void setGrid20Final(String[] grid20Final) {
+        this.grid20Final = grid20Final;
+    }
+
+    public String[] getGrid21Final() {
+        return grid21Final;
+    }
+
+    public void setGrid21Final(String[] grid21Final) {
+        this.grid21Final = grid21Final;
+    }
+
+    public String[] getGrid22Final() {
+        return grid22Final;
+    }
+
+    public void setGrid22Final(String[] grid22Final) {
+        this.grid22Final = grid22Final;
+    }
+
+    public String[] getGrid23Final() {
+        return grid23Final;
+    }
+
+    public void setGrid23Final(String[] grid23Final) {
+        this.grid23Final = grid23Final;
+    }
+
+    public String[] getGrid24Final() {
+        return grid24Final;
+    }
+
+    public void setGrid24Final(String[] grid24Final) {
+        this.grid24Final = grid24Final;
+    }
+
+    public String[] getGrid25Final() {
+        return grid25Final;
+    }
+
+    public void setGrid25Final(String[] grid25Final) {
+        this.grid25Final = grid25Final;
+    }
+
+    public String[] getGrid26Final() {
+        return grid26Final;
+    }
+
+    public void setGrid26Final(String[] grid26Final) {
+        this.grid26Final = grid26Final;
+    }
+
+    public String[] getGrid27Final() {
+        return grid27Final;
+    }
+
+    public void setGrid27Final(String[] grid27Final) {
+        this.grid27Final = grid27Final;
+    }
+
+    public String[] getGrid28Final() {
+        return grid28Final;
+    }
+
+    public void setGrid28Final(String[] grid28Final) {
+        this.grid28Final = grid28Final;
+    }
+
+    public String[] getGrid29Final() {
+        return grid29Final;
+    }
+
+    public void setGrid29Final(String[] grid29Final) {
+        this.grid29Final = grid29Final;
+    }
+
+    public String[] getGrid30Final() {
+        return grid30Final;
+    }
+
+    public void setGrid30Final(String[] grid30Final) {
+        this.grid30Final = grid30Final;
+    }
+
+    public String[] getGrid31Final() {
+        return grid31Final;
+    }
+
+    public void setGrid31Final(String[] grid31Final) {
+        this.grid31Final = grid31Final;
+    }
+
+    public String[] getGrid32Final() {
+        return grid32Final;
+    }
+
+    public void setGrid32Final(String[] grid32Final) {
+        this.grid32Final = grid32Final;
+    }
+
+    public String[] getGrid33Final() {
+        return grid33Final;
+    }
+
+    public void setGrid33Final(String[] grid33Final) {
+        this.grid33Final = grid33Final;
+    }
+
+    public String[] getGrid34Final() {
+        return grid34Final;
+    }
+
+    public void setGrid34Final(String[] grid34Final) {
+        this.grid34Final = grid34Final;
+    }
+
+    public String[] getGrid35Final() {
+        return grid35Final;
+    }
+
+    public void setGrid35Final(String[] grid35Final) {
+        this.grid35Final = grid35Final;
+    }
+
+    public String[] getGrid36Final() {
+        return grid36Final;
+    }
+
+    public void setGrid36Final(String[] grid36Final) {
+        this.grid36Final = grid36Final;
+    }
+
+    public String[] getGrid37Final() {
+        return grid37Final;
+    }
+
+    public void setGrid37Final(String[] grid37Final) {
+        this.grid37Final = grid37Final;
+    }
+
+    public String[] getGrid38Final() {
+        return grid38Final;
+    }
+
+    public void setGrid38Final(String[] grid38Final) {
+        this.grid38Final = grid38Final;
+    }
+
+    public String[] getGrid39Final() {
+        return grid39Final;
+    }
+
+    public void setGrid39Final(String[] grid39Final) {
+        this.grid39Final = grid39Final;
+    }
+
+    public String[] getGrid40Final() {
+        return grid40Final;
+    }
+
+    public void setGrid40Final(String[] grid40Final) {
+        this.grid40Final = grid40Final;
+    }
+
+    public String[] getGrid41Final() {
+        return grid41Final;
+    }
+
+    public void setGrid41Final(String[] grid41Final) {
+        this.grid41Final = grid41Final;
+    }
+
+    public String[] getGrid42Final() {
+        return grid42Final;
+    }
+
+    public void setGrid42Final(String[] grid42Final) {
+        this.grid42Final = grid42Final;
+    }
+
+    public String[] getGrid43Final() {
+        return grid43Final;
+    }
+
+    public void setGrid43Final(String[] grid43Final) {
+        this.grid43Final = grid43Final;
+    }
+
+    public String[] getGrid44Final() {
+        return grid44Final;
+    }
+
+    public void setGrid44Final(String[] grid44Final) {
+        this.grid44Final = grid44Final;
+    }
+
+    public String[] getGrid45Final() {
+        return grid45Final;
+    }
+
+    public void setGrid45Final(String[] grid45Final) {
+        this.grid45Final = grid45Final;
+    }
+
+    public String[] getGrid46Final() {
+        return grid46Final;
+    }
+
+    public void setGrid46Final(String[] grid46Final) {
+        this.grid46Final = grid46Final;
+    }
+
+    public String[] getGrid47Final() {
+        return grid47Final;
+    }
+
+    public void setGrid47Final(String[] grid47Final) {
+        this.grid47Final = grid47Final;
+    }
+
+    public String[] getGrid48Final() {
+        return grid48Final;
+    }
+
+    public void setGrid48Final(String[] grid48Final) {
+        this.grid48Final = grid48Final;
+    }
+
+    public String[] getGrid49Final() {
+        return grid49Final;
+    }
+
+    public void setGrid49Final(String[] grid49Final) {
+        this.grid49Final = grid49Final;
+    }
+
+    public String[] getGrid50Final() {
+        return grid50Final;
+    }
+
+    public void setGrid50Final(String[] grid50Final) {
+        this.grid50Final = grid50Final;
+    }
+}
